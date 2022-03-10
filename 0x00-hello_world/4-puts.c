0c93122baf5df99488eb0eeefee9bc0b25218538
oryankibandi <shreckoryan@gmail.com>
2022-03-10 14:32:47 +0300

    size of

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(){
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+/**
+ * main - DIsplays a message
+ * Return: Returns zero
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
-int main(){
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+/**
+ * main - prints test with printf
+ * Return: Return type is int
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
