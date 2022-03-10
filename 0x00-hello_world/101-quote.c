52810bf912082ee30c76151fc720881a7412b74f
oryankibandi <shreckoryan@gmail.com>
2022-03-10 16:25:56 +0300

    print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints a text on screen
+ * Return: This function returns 1
+*/
+
+int main(void)
+{
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
