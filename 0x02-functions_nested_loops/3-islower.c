156b5e301f3c3b4703393780250ca879ee325ed5
oryankibandi <shreckoryan@gmail.com>
2022-03-15 16:48:34 +0300

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,10 +5,10 @@
 */
 int _islower(int c)
 {
-	if(c >= 65 && c<=90)
+	if (c >= 65 && c <= 90)
 	{
 		return (0);
-	} else if (c >=97 && c<= 122)
+	} else if (c >= 97 && c <= 122)
 	{
 		return (1);
 	} else
