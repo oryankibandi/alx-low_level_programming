3b1f376e4e8b185ddacead32eaab1baef1c07838
oryankibandi <shreckoryan@gmail.com>
2022-03-15 22:39:33 +0300

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,11 +2,14 @@
 
 /**
  * past_late - displays time past late
+ * @i: parameter to be compared
  * Return: void
 */
-void past_late(void)
+void past_late(int i)
 {
-	for (b = 0; b < 4; b++)
+	int b, c, d;
+
+	for (b = 0; b < i; b++)
 	{
 		for (c = 0; c < 6; c++)
 		{
@@ -30,30 +33,16 @@ void past_late(void)
 */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int a;
 
 	for (a = 0; a < 3; a++)
 	{
 		if (a == 2)
 		{
-			past_late();
+			past_late(4);
 		} else
 		{
-			for (b = 0; b < 10; b++)
-			{
-				for (c = 0; c < 6; c++)
-				{
-					for (d = 0; d < 10; d++)
-					{
-						_putchar(a + '0');
-						 _putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			past_late(10);
 		}
 	}
 }
