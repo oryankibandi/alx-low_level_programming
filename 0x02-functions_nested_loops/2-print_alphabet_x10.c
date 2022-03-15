28ad00a0fe0d3ede6508dc9b753aa1678f5b87ee
oryankibandi <shreckoryan@gmail.com>
2022-03-15 15:10:56 +0300

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,8 +16,13 @@ void print_alphabet_x10(void)
 	{
 		for (j = 0; j < 27; j++)
 		{
-			_putchar(alphabet[j]);
+			if(alphabet[j] == '\0')
+			{
+				_putchar('\n');
+			}else
+			{
+				_putchar(alphabet[j]);
+			}
 		}
 	}
-	_putchar('\n');
 }
