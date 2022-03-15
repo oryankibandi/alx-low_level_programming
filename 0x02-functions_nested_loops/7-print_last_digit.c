bc7e8786778176a7a3473eec3627a8e056ad4835
oryankibandi <shreckoryan@gmail.com>
2022-03-15 20:39:55 +0300

    absolute

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of an int
+ *
+ * @i: digit to be checked
+ *
+ * Return: lastDigit
+*/
+int print_last_digit(int i)
+{
+	int lastDigit = i % 10;
+
+	if (lastDigit < 0)
+	{
+		lastDigit *= -1;
+	}
+	_putchar(lastDigit);
+	return (lastDigit);
+}
