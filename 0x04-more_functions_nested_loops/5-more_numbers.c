cc244cbc15a7b4e716f7f8cb3b31df6e446ca960
oryankibandi <shreckoryan@gmail.com>
2022-03-17 14:33:13 +0300

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,26 @@
 */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
+	int nums[] = {0, 0};
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			} else
+			{
+				nums[0] = j / 10;
+				nums[1] = j % 10;
+
+				for (k = 0; k < 2; k++)
+				{
+					_putchar(nums[k] + '0');
+				}
+			}
 		}
 		_putchar('\n');
 	}
