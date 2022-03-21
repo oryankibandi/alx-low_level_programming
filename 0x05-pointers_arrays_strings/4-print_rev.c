a3a61399a1bf0cb6b73b737d81c0ec27491405a4
oryankibandi <shreckoryan@gmail.com>
2022-03-21 14:30:43 +0300

    copy string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * stringlen - returns length of a string
+ * @s: Pointer to string
+ * Return: length
+*/
+int stringlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to tring to print
+ * Return: void
+*/
+void print_rev(char *s)
+{
+	int length = stringlen(s);
+	int i;
+
+	for (i = length; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
