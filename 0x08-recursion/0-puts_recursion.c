b1d37a3d55bb2e00061ee4fb306be80f4d6ac6bc
oryankibandi <shreckoryan@gmail.com>
2022-03-30 13:41:23 +0300

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,28 @@
 
 void print(char *s, int index);
 
+/**
+ * _puts_recursion - prints a character
+ * @s: pointer to string
+ * Return: void
+ */
 void _puts_recursion(char *s)
 {
 	int i = 0;
-	
+
 	print(s, i);
 	_putchar('\n');
 }
 
+/**
+ * print - prints a character
+ * @s: pointer to string
+ * @index: index
+ * Return: void
+ */
 void print(char *s, int index)
 {
-	if(s[index] != '\0')
+	if (s[index] != '\0')
 	{
 		_putchar(s[index]);
 		index = index + 1;
