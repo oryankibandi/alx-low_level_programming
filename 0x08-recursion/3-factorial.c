e5e455ba5f60e9ab520a7b478fb21e9f98ef4d6b
oryankibandi <shreckoryan@gmail.com>
2022-03-30 22:00:43 +0300

    prime number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial of a number
+ * @n: number
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
