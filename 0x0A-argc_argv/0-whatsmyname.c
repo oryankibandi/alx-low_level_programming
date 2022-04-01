150e17e4af2fd39c48b39f62845a591ab695fd00
oryankibandi <shreckoryan@gmail.com>
2022-04-01 14:28:42 +0300

    print args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array of arguments passed
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
