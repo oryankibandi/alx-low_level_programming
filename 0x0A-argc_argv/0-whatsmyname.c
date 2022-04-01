2626baefbf951c7b105ef4f447fc2ff9f819b416
oryankibandi <shreckoryan@gmail.com>
2022-04-01 12:18:44 +0300

    print nae of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments in argv[] array
+ * @argv: array of arguments passed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+## ARGV and ARGC
+
+This repo contins code on implementing argv and argc in C.
