28a23efc10beded599f5c34ec7da42614f14c3ed
oryankibandi <shreckoryan@gmail.com>
2022-04-04 12:39:52 +0300

    create array dynamically

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array dynamically and assigns the character c
+ * @c: character to assign
+ * @size: size of array
+ *
+ * Return: pointer to array or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(size);
+	if (arr)
+	{
+		for (i = 0; i < size; i++)
+		{
+			arr[i] = c;
+		}
+
+		return (arr);
+	}
+
+	return (NULL);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+## Malloc, Free
+
+This directory contains use cases of malloc() and free() for dynamic memory allocation in C.
