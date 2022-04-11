5f0ee444c3c19a77196b98dba18ed0a23b51156f
oryankibandi <shreckoryan@gmail.com>
2022-04-11 22:13:59 +0300

    initialize a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
+ *
+ * This struct represents a dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
