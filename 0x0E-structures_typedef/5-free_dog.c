5e527fd14795237723ee636b492d3268a8c27687
oryankibandi <shreckoryan@gmail.com>
2022-04-11 23:30:58 +0300

    fre

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * free_dog - frees the dog memory location
+ * @d: pointer to dog struct
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d->age);
+		free(d);
+	}
+}
