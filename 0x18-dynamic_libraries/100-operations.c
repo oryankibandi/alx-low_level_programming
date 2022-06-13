1df43ad3d032024375060eb5e428f21ab1d738c7
oryankibandi <shreckoryan@gmail.com>
2022-06-13 16:08:05 +0300

    complete tasks

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * add - adds two ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
+
+/**
+ * sub - subtracts two ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	int diff = a - b;
+
+	return (diff);
+}
+
+/**
+ * mul - multplies two ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int prod = a * b;
+
+	return (prod);
+}
+
+/**
+ * div - divides two ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: quotient
+ */
+int div(int a, int b)
+{
+	int quo = a / b;
+
+	return (quo);
+}
+
+/**
+ * mod - gets the modulus of two ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: rem
+ */
+int mod(int a, int b)
+{
+	int rem = a % b;
+
+	return (rem);
+}
