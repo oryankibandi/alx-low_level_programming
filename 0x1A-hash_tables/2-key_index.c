f2f08810db5712ee6c936673714430e5ac30245b
oryankibandi <shreckoryan@gmail.com>
2022-07-01 23:27:03 +0300

    fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,7 +10,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash = hash_djb2(key);
-	unsigned long int index = hash * size;
+	unsigned long int index = hash % size;
 
 	return (index);
 }
